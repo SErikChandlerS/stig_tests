d72faf55def2121b3546cdf40f1dd2425b4174d3
SErikChandlerS <43161679+SErikChandlerS@users.noreply.github.com>
2023-06-10 19:39:13 +0300

    Update process_request.py

--- process_request.py
+++ process_request.py
@@ -3,6 +3,7 @@ import zipfile
 import argparse
 import smtplib
 
+from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email.mime.base import MIMEBase
 from email import encoders
